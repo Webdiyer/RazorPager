242f857d1b1592f57075df73c43723061c9f167f
Webdiyer <webdiyer@gmail.com>
2020-12-13 11:23:21 +0800

    Add project files.

---
+++ Src/RazorPager.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Webdiyer
+{
+    [HtmlTargetElement("razorpager")]
+    public class RazorPager:TagHelper
+    {
+        private HtmlHelper _htmlHelper;
+        private HtmlEncoder _htmlEncoder;
+        private IUrlHelperFactory _urlHelperFactory;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public RazorPager(IHtmlHelper htmlHelper,HtmlEncoder htmlEncoder,IUrlHelperFactory urlHelperFactory)
+        {
+            _htmlHelper = htmlHelper as HtmlHelper;
+            _htmlEncoder = htmlEncoder;
+            _urlHelperFactory = urlHelperFactory;
+        }
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            (_htmlHelper as IViewContextAware).Contextualize(ViewContext);
+            output.TagName = "razorpager";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            var rvalue = ViewContext.RouteData.Values;
+            var qryPrms =ViewContext.HttpContext.Request.Query.ToDictionary(d => d.Key, d => d.Value.ToString());
+            if (qryPrms != null && qryPrms.Any())
+            {
+                foreach (var kv in qryPrms)
+                {
+                    if (!rvalue.ContainsKey(kv.Key))
+                    {
+                        rvalue.Add(kv.Key, kv.Value);
+                    }
+                }
+            }
+            int pageIndex;
+            string strIndex = rvalue.ContainsKey(PageIndexParameterName) ? rvalue[PageIndexParameterName].ToString() : ViewContext.HttpContext.Request.Query[PageIndexParameterName].ToString();
+            if (!int.TryParse(strIndex, out pageIndex))
+            {
+                pageIndex = 1;
+            }
+            //如果当前页超出总页数，则重置当前页为最后一页
+            while (TotalItemCount > 0 && TotalItemCount <= (pageIndex - 1) * PageSize)
+            {
+                pageIndex--;
+            }
+            if (pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            var metaData = new RazorPagerMetaData(TotalItemCount, pageIndex, PageSize, PageIndexParameterName, rvalue, MaxPagerItems, urlHelper);
+            var partial = await _htmlHelper.PartialAsync(TemplateName,metaData);
+            var writer = new StringWriter();
+            partial.WriteTo(writer, _htmlEncoder);
+            output.Content.SetHtmlContent(writer.ToString());
+        }
+
+        #region properties
+        public string TemplateName { get; set; } = "Basic";
+
+        public int MaxPagerItems { get; set; } = 10;
+
+        public int TotalItemCount { get; set; }
+
+        public int PageSize { get; set; } = 10;
+
+        public string PageIndexParameterName { get; } = "pageindex";
+
+        #endregion
+    }
+}
---
+++ Src/RazorPagerMetaData.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+
+namespace Webdiyer
+{
+    public class RazorPagerMetaData
+    {
+        public int TotalItemCount { get; set; }
+
+        public int CurrentPageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalPageCount => (int)Math.Ceiling(TotalItemCount / (double)PageSize);
+
+        public List<PagerItem> PagerItems;
+
+        private RouteValueDictionary _routeValues { get; }
+
+        private IUrlHelper _urlHelper { get; }
+
+        public string PageIndexParameterName { get; }
+
+        public bool HasPreviousPage => CurrentPageIndex > 1;
+
+        public bool HasNextPage => CurrentPageIndex < TotalPageCount;
+
+        public bool IsFirstPage => CurrentPageIndex == 1;
+
+        public bool IsLastPage => CurrentPageIndex == TotalPageCount;
+
+        public string FirstPageUrl => generatePaginationUrl(1);
+
+        public string UrlTemplate => generatePaginationUrl(int.MinValue).Replace(int.MinValue.ToString(), "__" + PageIndexParameterName + "__");
+
+        public string NextPageUrl
+        {
+            get
+            {
+                var nextPageIndex = CurrentPageIndex + 1;
+                if (nextPageIndex > TotalPageCount)
+                {
+                    nextPageIndex = TotalPageCount;
+                }
+                return generatePaginationUrl(nextPageIndex);
+            }
+        }
+
+        public string PreviousPageUrl
+        {
+            get
+            {
+                var prevPageIndex = CurrentPageIndex - 1;
+                if (prevPageIndex < 1)
+                {
+                    prevPageIndex = 1;
+                }
+                var url = generatePaginationUrl(prevPageIndex);
+                return url;
+            }
+        }
+
+        public string LastPageUrl => generatePaginationUrl(TotalPageCount);
+
+        public string CurrentPageUrl => generatePaginationUrl(CurrentPageIndex);
+
+        string generatePaginationUrl(int pageIndex)
+        {
+            var rv = new RouteValueDictionary(_routeValues);
+            rv[PageIndexParameterName] = pageIndex;
+            return _urlHelper.Page(_routeValues["page"] as string, rv);
+        }
+
+
+        public RazorPagerMetaData(int total, int pageIndex, int pageSize, string pageParameterName, RouteValueDictionary routeValues, int maxPagerItems, IUrlHelper urlHelper)
+        {
+            TotalItemCount = total;
+            CurrentPageIndex = pageIndex;
+            PageSize = pageSize;
+            _urlHelper = urlHelper;
+            _routeValues = routeValues;
+            PageIndexParameterName = pageParameterName;
+
+            var _startPageIndex = CurrentPageIndex - (maxPagerItems / 2);
+            if (_startPageIndex + maxPagerItems > TotalPageCount)
+            {
+                _startPageIndex = TotalPageCount + 1 - maxPagerItems;
+            }
+            if (_startPageIndex < 1)
+            {
+                _startPageIndex = 1;
+            }
+
+            // end page index
+            var _endPageIndex = _startPageIndex + maxPagerItems - 1;
+            if (_endPageIndex > TotalPageCount)
+            {
+                _endPageIndex = TotalPageCount;
+            }
+            PagerItems = new List<PagerItem>();
+            for (int i = _startPageIndex; i <= _endPageIndex; i++)
+            {
+                routeValues[pageParameterName] = i;
+                PagerItems.Add(new PagerItem(i, urlHelper.Page(routeValues["page"] as string, routeValues), i == CurrentPageIndex));
+            }
+        }
+    }
+
+    public class PagerItem
+    {
+        public int PageIndex { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsCurrent { get; set; }
+
+        public PagerItem(int pageIndex, string url, bool isCurrent)
+        {
+            PageIndex = pageIndex;
+            Url = url;
+            IsCurrent = isCurrent;
+        }
+    }
+}
